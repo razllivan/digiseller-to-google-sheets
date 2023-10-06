9a8bad5ae1d228dc4f98beb2ac7445727d48042b
ivan <shanin-i2011@yandex.ru>
2023-10-06 17:32:10 +0300

    add settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+CREDENTIALS_PATH = os.path.join(BASE_DIR, 'service_account.json')
+
+# Environment variables
+SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1IS8SeVZzij9Y0S71ZnJJ1msg70tiuX90fevL-add10o/edit?usp=sharing'
+WORKSHEET_NAME = 'Товары'
