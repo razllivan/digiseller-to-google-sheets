2ed7029815e64ec081de2924aa82ec909d033cef
ivan <shanin-i2011@yandex.ru>
2023-10-09 00:04:34 +0300

    refactor DigisellerAPI init method for improve functionality

--- api/digiseller_api.py
+++ api/digiseller_api.py
@@ -1,8 +1,18 @@
+import requests
+
+
 class DigisellerAPI:
-    def __init__(self):
-        self.base_url = "https://api.digiseller.ru/api/"
+    BASE_URL = "https://api.digiseller.ru/api/"
+
+    def __init__(self, api_key: str, seller_id: int):
+        if not api_key or not seller_id:
+            raise ValueError("API key and seller id are required")
+
+        self.api_key = api_key
+        self.seller_id = seller_id
         self.token = None
         self.token_expiration = None
+        self.session = requests.Session()
 
     def get_products(self):
         # TODO: Implement get_products method
