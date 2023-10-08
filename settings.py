13890dc57c02e977e278bd4ffbdf4922f8801eb9
ivan <shanin-i2011@yandex.ru>
2023-10-08 20:53:01 +0300

    move environment variables to .env file

--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 CREDENTIALS_PATH = os.path.join(BASE_DIR, 'service_account.json')
 
 # Environment variables
-SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1IS8SeVZzij9Y0S71ZnJJ1msg70tiuX90fevL-add10o/edit?usp=sharing'
-WORKSHEET_NAME = 'Товары'
+load_dotenv()
+SPREADSHEET_URL = os.environ.get('SPREADSHEET_URL')
+WORKSHEET_NAME = os.environ.get('WORKSHEET_NAME')
