b43adea33d2fc4551ca6454888770cb350d7e8d9
ivan <shanin-i2011@yandex.ru>
2023-10-11 00:30:19 +0300

    clean up todo-methods

--- api/digiseller_api.py
+++ api/digiseller_api.py
@@ -1,5 +1,7 @@
 from hashlib import sha256
+
 import requests
+
 BASE_URL = "https://api.digiseller.ru/api/"
 
 
@@ -15,9 +17,6 @@ class DigisellerAPI:
         self.token_expiration = None
         self.session = requests.Session()
 
-    def get_products(self):
-        # TODO: Implement get_products method
-        pass
     def generate_sign(self, timestamp: str) -> str:
         """
           Generate a digiseller sign using the API key and timestamp.
@@ -30,11 +29,3 @@ class DigisellerAPI:
         concatenated_string = f"{self.api_key}{timestamp}"
         byte_array = concatenated_string.encode('utf-8')
         return sha256(byte_array).hexdigest()
-
-    def update_product(self, product):
-        # TODO: Implement update_product method
-        pass
-
-    def check_changes(self):
-        # TODO: Implement check_changes method
-        pass
